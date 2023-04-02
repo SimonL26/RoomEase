a89924ab9883a55294b05580e87bb04d466f0f8a
SimonL26 <806985104@qq.com>
2023-04-02 19:10:18 +0100

    adding database connection with sequelize to postgres db

--- server/RoomEase/index.ts
+++ server/RoomEase/index.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import { sequelizeConnection } from "./src/configs/db.config";
 
 dotenv.config();
 
-const app: Express = express();
+const app: Application = express();
 const port = 5000;
 
 app.use(bodyParser.json());
@@ -18,6 +19,11 @@ app.get('/', (req: Request, res: Response) => {
     res.json({info: 'Testing Express js + Typescript and Postgres test reload'})
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`App running on port ${port}`)
+    sequelizeConnection.authenticate().then(() => {
+        console.log("Connected to database roomease")
+    }).catch((err: any) => {
+        console.log(err.message)
+    })
 })
--- server/RoomEase/src/configs/db.config.ts
+++ server/RoomEase/src/configs/db.config.ts
@@ -1,1 +1,16 @@
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const host = process.env.PGHOST as string;
+const port = parseInt(process.env.PORT || "5432") as number;
+const username = process.env.PGUSER as string;
+const password = process.env.PGPASSWORD as string;
+const dbname = process.env.PGDATABSE as string;
+
+export const sequelizeConnection = new Sequelize(dbname, username, password, {
+    host: host,
+    port: port,
+    dialect: 'postgres',
+})
